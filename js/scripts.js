27d24543c3461ce692c55aa5bffb7bac614ce62c
Raphael Katana <roduor41@gmail.com>
2018-09-22 07:44:32 +0300

    add if and ese statement for section 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,31 @@ var score=function(q1,q2,q3,q4,q5){
   return (q1 + q2 + q3 + q4 + q5);
 }
 
-var section1=function
+var section1=function(name1, name2, number, email){
+  return (name1+name2+number+email);
+}
 
 //User interface logic
+$(document).ready(function(){
+  $("#check").click(function(event){
+    event.preventDefault();
+    var name1=parseInt($(name1).val());
+    var name2=parseInt($(name2).val());
+    var number=parseInt($(number).val());
+    var email=parseInt($(email).val());
+    var confirm=section1(name1,name2,number,email);
+    var confirm=20;
+
+    if (confirm!=20){
+      alert("Please ensure you fill all the required fields");
+    }
+    else{
+      alert("Proceed to section Two")
+    }
+  })
+
+
+})
 $(document).ready(function() {
   $("#sub").click(function(event) {
     var q1 = parseInt($("input:radio[name=one]:checked").val());
